92006df0140c064a4a275c637c05b75ed746c7b5
danieloflynn <danielpoflynn@gmail.com>
2023-12-03 15:03:53 +0000

    Added Orc and Goblin class

--- Creatures.py
+++ Creatures.py
@@ -2,16 +2,17 @@ from random import randint
 
 
 class Creature:
+    default_abilities = {
+        "attack": 1,
+        "defence": 5,
+        "speed": 5
+    }
 
-    def __init__(self, name: str, hp: int = 10):
+    def __init__(self, name: str, hp: int = 10, abilities=default_abilities):
         self.name = name
         self.HP = hp
         self.maxHP = hp
-        self.abilities = {
-            "attack": 1,
-            "defence": 5,
-            "speed": 5
-        }
+        self.abilities = abilities
 
     def get_name(self):
         return self.name
@@ -56,7 +57,6 @@ class Creature:
             return
 
         rand = randint(0, len(alive_targets)-1)
-        # TODO: Make sure this actually returns the target class, not a new instance
         return alive_targets[rand]
 
     def turn(self, round_num, target_list):
@@ -69,3 +69,67 @@ class Creature:
 
     def __repr__(self):
         return self.__str__()
+
+
+class Goblin(Creature):
+    default_abilities = {
+        "attack": 3,
+        "defence": 6,
+        "speed": 3
+    }
+
+    def __init__(self, name: str, hp: int = 15, abilities=default_abilities):
+        super().__init__(self, name, hp, abilities)
+
+
+class Orc(Creature):
+    default_abilities = {
+        "attack": 5,
+        "defence": 8,
+        "speed": 3
+    }
+
+    default_rage = {
+        "attack": 5,
+        "defence": -3,
+        "speed": 0
+    }
+
+    def __init__(self, name: str, hp: int = 50, abilities=default_abilities, rage_abilities=default_rage):
+        self.name = name
+        self.HP = hp
+        self.maxHP = hp
+        self.abilities = abilities
+        self.rage_abilities = rage_abilities
+        self.inRage = 0
+
+    def get_attack(self):
+        return self.abilities["attack"] + self.inRage * self.rage_abilities["attack"]
+
+    def get_defence(self):
+        return self.abilities["defence"] + self.inRage * self.rage_abilities["defence"]
+
+    def get_speed(self):
+        return self.abilities["speed"] + self.inRage * self.rage_abilities["speed"]
+
+    def attack(self, target):
+        if self.inRage == 1:
+            print(f"{self.name} cooled down.")
+            self.inRage = 0
+
+        super().attack(target)
+
+    def heavy_attack(self, target):
+        if self.inRage == 0:
+            print(f"{self.name} is in rage.")
+            self.inRage == 1
+
+        super().attack(target)
+
+    def turn(self, round_num, target_list):
+        target = self.auto_select(target_list)
+        if target:
+            if round_num % 4 != 0:
+                self.attack(target)
+            else:
+                self.heavy_attack(target)
--- unittest.py
+++ unittest.py
@@ -5,18 +5,23 @@ import random
 # Check that get methods work
 c1 = Creature("Test1")
 
+print("Testing get methods...")
 assert c1.get_name() == "Test1", "Should be Test1"
 assert c1.check_life() == 10, "Should be 10"
 assert c1.get_attack() == 1, "Should be 1"
 assert c1.get_defence() == 5, "Should be 5"
 assert c1.get_speed() == 5, "Should be 5"
+print("Get methods passed")
 
+print("Testing life assignment...")
 c2 = Creature("Test", 20)
 assert c2.check_life() == 20, f"Life should be 20, is {c2.check_life()}"
+print("Passed.")
 
 # Test 2
 # Check reduce life method
 # Can reduce life
+print("Testing reduce life methods.")
 c1.reduce_life(1)
 assert c1.check_life() == 9, "Life should be 9"
 
@@ -31,5 +36,9 @@ assert c1.check_life() == 0, "Life should be 0"
 # Can reduce life on a creature with custom health
 c2.reduce_life(12)
 assert c2.check_life() == 8, f"Life should be 8, is {c2.check_life()}"
+print("Reduce life methods passed.")
+
+# Test 3 - test attack and auto select
+
 
 print("All unit tests ok")
