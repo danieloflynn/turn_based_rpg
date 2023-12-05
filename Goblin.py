b72151c6e541448927b7d71942ebbca2172a0808
danieloflynn <danielpoflynn@gmail.com>
2023-12-05 18:38:04 +0000

    Added comments to Goblin

--- Goblin.py
+++ Goblin.py
@@ -2,6 +2,10 @@ from Creatures import Creature
 
 
 class Goblin(Creature):
+    """Goblin Class. Inherits from Creature.
+    Class Attributes:
+    - default_attributes (dict[str, int]): Default attributes for this class, to be used if none supplied in constructor.
+    """
     default_abilities = {
         "attack": 3,
         "defence": 6,
@@ -9,4 +13,11 @@ class Goblin(Creature):
     }
 
     def __init__(self, name: str, hp: int = 15, abilities: dict[str, int] = default_abilities):
+        """Constructor for Goblin. Essentially the same as Creature except different default health and different default abilities.
+
+        Args:
+            name (str): Name of Goblin.
+            hp (int, optional): Starting/Max HP of Goblin. Defaults to 15.
+            abilities (_type_, optional): Dictionary containing the abilities of the Goblin (attack, defence, and speed).. Defaults to default_abilities.
+        """
         Creature.__init__(self, name, hp, abilities)
